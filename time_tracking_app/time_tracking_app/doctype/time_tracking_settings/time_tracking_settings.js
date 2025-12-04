5f4ed9ca5d600b0844caf9012f0b3b028c26e827
Horst Gruber <horst.gruber@ransoft.at>
2025-12-04 06:18:48 +0000

    added doctypes

---
+++ time_tracking_app/time_tracking_app/doctype/time_tracking_settings/time_tracking_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2025, RAN Soft GmbH & Co KG and contributors
+// For license information, please see license.txt
+
+// frappe.ui.form.on("Time Tracking Settings", {
+// 	refresh(frm) {
+
+// 	},
+// });
